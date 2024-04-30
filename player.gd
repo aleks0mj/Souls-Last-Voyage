77567dba6aa6c964d37c41a6e21eafc5529e3620
AleksMJ <167433905+Aleks0MJ@users.noreply.github.com>
2024-04-30 16:58:53 +0200

    Added spawn, space, void, music
    Added island assets, started spawn island, added space background, added void, added music.
    ---
    Story(?):
    The world has been destroyed by “The Void”, there’s only pieces of what is left of Earth, and you’re a lost soul that needs to jump up to “Heaven” on the pieces of Earth. If you fall into The Void, you lose your soul.

--- player.gd
+++ player.gd
@@ -5,25 +5,25 @@ const JUMP_VELOCITY = -400.0
 const FRICTION = 3000  # Ground friction
 const AIR_RESISTANCE = 2000  # Air friction
 
-# Get the gravity from the project settings
+# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		# Play jump animation if moving upwards and if not already playing
+		# Play the jump animation if moving upwards and it's not already playing.
 		if velocity.y < 0 and $AnimatedSprite2D.animation != "jump":
 			$AnimatedSprite2D.animation = "jump"
 			$AnimatedSprite2D.play()
 
-	# Handle jump input
+	# Handle jump input.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		$AnimatedSprite2D.animation = "jump"
 		$AnimatedSprite2D.play()
 
-	# Get the input direction and handle the movement/deceleration
+	# Get the input direction and handle the movement/deceleration.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -32,7 +32,7 @@ func _physics_process(delta):
 			$AnimatedSprite2D.play()
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 	else:
-		# Apply ground friction to stop character when no input detected
+		# Apply ground friction to stop the character when no input is detected
 		if is_on_floor():
 			velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 			if velocity.x == 0:
@@ -40,4 +40,4 @@ func _physics_process(delta):
 				$AnimatedSprite2D.play()
 
 	# Apply movement
-	move_and_slide()
+	move_and_slide()  # No parameters needed in Godot 4.0 for CharacterBody2D
